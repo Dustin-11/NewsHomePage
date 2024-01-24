95843ca4bb36f80048cfb9df34e51c0b3165894d
Dustin Kurtz <11kurtzd@gmail.com>
2024-01-24 14:46:38 -0500

    Update

--- newshomepage/src/Components/Header.jsx
+++ newshomepage/src/Components/Header.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
 
 const Header = () => {
+    const [isChecked, SetChecked] = useState(false);
     return (
         <>
-            <div>
-                <inuput type="checkbox" id="hamburger-checkbox" className="hamburger-checkbox" />
+            <div className="test">
+                <input type="checkbox" id="hamburger-checkbox" className="hamburger-checkbox"
+                    checked={isChecked}
+                />
                 <label htmlFor="hamburger-checkbox" className="hamburger-label">
                     <span className="hamburger-icon">&#9776;</span>
                 </label>
